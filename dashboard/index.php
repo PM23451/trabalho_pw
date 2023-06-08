6f741d5303a1e488a202a1f7fd82b2a429e4f262
PM23451 <pedro.miranda@ipvc.pt>
2023-06-08 15:29:46 +0100

    connection

--- dashboard/index.php
+++ dashboard/index.php
@@ -1,4 +1,5 @@
 
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -20,7 +21,7 @@
   </head>
   <body class="text-center">
     <form class="form-signin">
-      <img class="mb-4" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
+      <img class="mb-4" src="../assets/img/brasao-marinhas.webp" alt="" width="72" height="72">
       <h1 class="h3 mb-3 font-weight-normal">Dashboard</h1>
       <input type="email" id="inputEmail" class="form-control" placeholder="Email" required autofocus>
       <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
